1b9af383a34837cd0fe8f1234d5ce6f177dbc4db
Remo Koch <kochremo@gmail.com>
2017-06-08 18:52:02 +0200

    rendered city snippets

--- views/continent.php
+++ views/continent.php
@@ -4,62 +4,34 @@
         ?>
         <div class="continentInfos">
             <p class="title">Most popular Cities in North America</p>
-            <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="images/newyork1.PNG">
-                </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">New York<i class="material-icons right">more_vert</i></span>
-                    <p><a href="cities/newyork">More Infos</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">New York<i class="material-icons right">close</i></span>
-                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="images/sanfrancisco1.PNG">
-                </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">San Francisco<i class="material-icons right">more_vert</i></span>
-                    <p><a href="#">More Infos</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">New York<i class="material-icons right">close</i></span>
-                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="images/miami1.PNG">
-                </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">Miami<i class="material-icons right">more_vert</i></span>
-                    <p><a href="#">More Infos</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">New York<i class="material-icons right">close</i></span>
-                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                </div>
-            </div>
-            <div style="color:white"><?php 
 
-            echo($continent);
-            echo($category);
-            echo("</br>");
-            echo("data/".$continent.".csv");
-            echo("</br>");
+            <?php 
+
             $array = loadCSV("data/".$continent.".csv");
-            $index = getIndexOf("Nature",$array);
+            $index = getIndexOf($category,$array);
             $array = getSortedArray($index, $array);
-            
-            echo("-</br>");
+
             foreach($array as &$city) {
-                echo($city["name"]."&emsp;&emsp;&emsp;&emsp;&emsp;".$city["rank"]);
-                echo("</br>");
+                //echo($city["name"]."&emsp;&emsp;&emsp;&emsp;&emsp;".$city["rank"]);
+                ?>
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="images/miami1.PNG">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4"><?php echo($city["name"])?><i class="material-icons right">more_vert</i></span>
+                        <p><a href="#">More Infos</a></p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">New York<i class="material-icons right">close</i></span>
+                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                    </div>
+                </div>
+                    
+                <?php
             }
-            ?></div>
+            ?>
+            
         </div>
         <div class="continentBreak"> </div>
 
